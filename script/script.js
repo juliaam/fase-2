1889d60f5dffda3a2bd1f139273ce00a187b4c38
julia moraes <juliademoraess@gmail.com>
2023-11-25 03:26:40 -0300

    feat: projeto

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-<script></script>
+
+
+// function para exibir a mensagem temporária de boas vindas
+function exibirBoasVindas() {
+    var quadradoMensagem = document.createElement('div');
+    quadradoMensagem.className = 'mensagem-quadrado';
+    quadradoMensagem.innerHTML = '<p>Bem-vindo ao nosso Pet Shop! Esperamos que você tenha uma ótima experiência.</p>';
+    document.body.appendChild(quadradoMensagem);
+
+    // Configurar um timeout para remover o quadrado após 5 segundos (5000 milissegundos)
+    setTimeout(function () {
+        quadradoMensagem.remove();
+    }, 5000);
+
+    // Exibir o quadrado
+    quadradoMensagem.style.display = 'block';
+}
+exibirBoasVindas()
